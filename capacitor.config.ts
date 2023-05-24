a9e3e8ef1ffd6394f86ce8a087206db309c79922
ccaparas666 <carmelo.caparas@lccdo.edu.ph>
2023-05-24 15:55:53 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Metric-Conversion',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
